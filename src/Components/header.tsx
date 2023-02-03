1806765a6859baa3ed02f975bebbe3a6b8486584
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-03 14:06:59 +0100

    Basic tool structure

---
+++ src/Components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header: React.FC<{}> = (): JSX.Element => {
+	return (
+		<>
+			<div className="Header">
+				<h1>Sulfide & Sulfate Soils</h1>
+				<h1>Classification Tool</h1>
+			</div>
+		</>
+	);
+};
---
+++ src/Pages/tool.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../Components/header';
+
+export const Tool: React.FC<{}> = (): JSX.Element => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
