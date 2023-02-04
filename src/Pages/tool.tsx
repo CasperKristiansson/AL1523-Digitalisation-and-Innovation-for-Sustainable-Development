dd038304748d40a98d9a6097d5faeb35dad8d846
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-04 20:00:18 +0100

    Specify amount of samples

--- src/Pages/tool.tsx
+++ src/Pages/tool.tsx
@@ -31,9 +31,25 @@ const useStyles = createUseStyles({
 export const Tool: React.FC<{}> = (): JSX.Element => {
 	const classes = useStyles();
 
+	//
+
 	const [loading, setLoading] = React.useState(false);
 	const [showResult, setShowResult] = React.useState(false);
-	const [state, setState] = React.useState({
+	const [numSamples, setNumSamples] = React.useState(1);
+	// Instead of having one state like this:
+	// const [state, setState] = React.useState({
+	// 	location: "",
+	// 	depth: "",
+	// 	surfaceType: "",
+	// 	s: 0,
+	// 	ca: 0,
+	// 	fe: 0,
+	// 	pHinit: 0,
+	// 	pHox: 0,
+	// } as ClassificationState);
+	// Keep track of a array of states. The standard should be one empty
+	// state, and then add more states as needed.
+	const [states, setStates] = React.useState([{
 		location: "",
 		depth: "",
 		surfaceType: "",
@@ -42,44 +58,85 @@ export const Tool: React.FC<{}> = (): JSX.Element => {
 		fe: 0,
 		pHinit: 0,
 		pHox: 0,
-	} as ClassificationState);
+	} as ClassificationState]);
+	// This function will be passed to the Input component, and will be
+	// called when the user changes the value of an input.
+	const setState = (index: number, state: ClassificationState) => {
+		// Create a copy of the states array.
+		const newStates = [...states];
+		// Replace the state at the given index with the new state.
+		newStates[index] = state;
+		// Update the states array.
+		setStates(newStates);
+	};
+
+	const handleNumSamplesChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		const newNumSamples = parseInt(event.target.value);
+		const newStates = [...states];
+		if (newNumSamples > numSamples) {
+			for (let i = numSamples; i < newNumSamples; i++) {
+				newStates.push({
+					location: "",
+					depth: "",
+					surfaceType: "",
+					s: 0,
+					ca: 0,
+					fe: 0,
+					pHinit: 0,
+					pHox: 0,
+				} as ClassificationState);
+			}
+		}
+		else if (newNumSamples < numSamples) {
+			for (let i = numSamples; i > newNumSamples; i--) {
+				newStates.pop();
+			}
+		}
+		setNumSamples(newNumSamples);
+		setStates(newStates);
+	};
 
 	const [result, setResult] = React.useState({} as Classification);
 
-	const handleAnalyzeClick = () => {
-		if (
-			!state.location ||
-			!state.depth ||
-			!state.surfaceType ||
-			state.s === 0 ||
-			state.ca === 0 ||
-			state.fe === 0 ||
-			state.pHinit === 0 ||
-			state.pHox === 0
-		) {
-			alert("Please fill all fields");
-			return;
-		}
+	// const handleAnalyzeClick = () => {
+	// 	if (
+	// 		!state.location ||
+	// 		!state.depth ||
+	// 		!state.surfaceType ||
+	// 		state.s === 0 ||
+	// 		state.ca === 0 ||
+	// 		state.fe === 0 ||
+	// 		state.pHinit === 0 ||
+	// 		state.pHox === 0
+	// 	) {
+	// 		alert("Please fill all fields");
+	// 		return;
+	// 	}
 
-		setLoading(true);
-		setTimeout(() => {
-			setLoading(false);
-			setShowResult(true);
+	// 	setLoading(true);
+	// 	setTimeout(() => {
+	// 		setLoading(false);
+	// 		setShowResult(true);
 
-			setResult(computeClassification(state));
+	// 		setResult(computeClassification(state));
 
-		}, 350);
-	};
+	// 	}, 350);
+	// };
 
 	return (
 		<>	
 			<Loader show={loading} />
 			<Header />
-			<Input setState={setState} state={state}/>
+			<Input setState={setState} numSamples={numSamples} states={states} />
 			<div className={classes.ButtonWrapper}>
-				<button className={classes.Button} onClick={handleAnalyzeClick}>
+				<button className={classes.Button} >
 					Analyze
 				</button>
+				<select value={numSamples} onChange={handleNumSamplesChange}>
+					{Array.from({length: 10}, (_, i) => i + 1).map(i => (
+					<option key={i} value={i}>{i}</option>
+					))}
+				</select>
 			</div>
 			{showResult && (
 				<Result
