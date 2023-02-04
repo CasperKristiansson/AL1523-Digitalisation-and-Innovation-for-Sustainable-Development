12a5af3a5cceb4f16c872250964d8a752d09531d
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-02-04 08:57:54 +0100

    Import component

--- src/Pages/tool.tsx
+++ src/Pages/tool.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from '../Components/header';
+import { Input } from '../Components/input';
 
 export const Tool: React.FC<{}> = (): JSX.Element => {
 	return (
 		<>
 			<Header />
+			<Input />
 		</>
 	);
 };
